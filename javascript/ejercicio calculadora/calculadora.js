6e43aca089b354e04e29864373862d59250f8189
dariorod <dario2303@gmail.com>
2022-04-17 13:21:33 -0300

    mi primer version

---
+++ javascript/ejercicio calculadora/calculadora.js
@@ -0,0 +1,65 @@
+//MULTIPLICACION CON FOR//
+
+// let multiplicando = 20; 
+// let multiplicador = 10;
+// let resultado = 0;
+    
+// for (i=0; i < multiplicador; i++) {
+
+//         if (i < multiplicador) {
+//             resultado = resultado + multiplicando;
+//         }
+//     }
+//     console.log("Resultado de la multiplicación: ", resultado);
+
+
+//MULTIPLICACION CON FUNCTION//
+
+// let resultado = 0;
+
+// function multiplicacion (multiplicando, multiplicador) {
+    
+//     for (i=0; i < multiplicador; i++) {
+
+//             if (i < multiplicador) {
+//                 resultado = resultado + multiplicando;
+//             }
+//         }
+//         console.log("Resultado de la multiplicación: ", resultado);
+// }
+
+// multiplicacion (40,10);
+// multiplicacion (80,90);
+
+
+
+//DIVISION CON FOR//
+// let dividendo = 2248; 
+// let divisor = 9;
+    
+// for (resultado = 0; divisor < (dividendo - divisor) ; resultado++) {
+
+//         if (divisor < (dividendo - divisor)) {
+//             dividendo = dividendo - divisor;
+//         }
+
+//     }    
+
+//     console.log("Resultado de la division: ", resultado);
+//     console.log("Resto de la división: ", dividendo % divisor); 
+
+function division (dividendo, divisor) {
+    for (resultado=0; divisor < (dividendo - divisor); resultado++) {
+        .
+        if (divisor < (dividendo - divisor)) {
+            dividendo = dividendo - divisor;
+            
+        }
+    }
+    console.log("Resultado de la division: ", resultado);
+    console.log("Resto de la división: ", dividendo % divisor); 
+}
+
+
+
+    division (20,2);
